368a04c484922bcf17f4423e328abfdb251d69df
Inguz001 <inguz01@gmail.com>
2026-06-18 16:37:10 -0500

    Prueba de secrets

--- database/postgres.py
+++ database/postgres.py
@@ -1,23 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 import streamlit as st
-import os
 
-# Primero intenta leer desde Streamlit Secrets
-try:
-    DATABASE_URL = st.secrets["DATABASE_URL"]
-except Exception:
-    # Si no existe (ejecución local), usa .env
-    from dotenv import load_dotenv
-    load_dotenv()
-    DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = st.secrets.get("DATABASE_URL")
 
-print("SECRETS:", st.secrets)
-print("DATABASE_URL:", st.secrets.get("DATABASE_URL"))
+if DATABASE_URL is None:
+    raise Exception(
+        f"DATABASE_URL no encontrada. Secrets disponibles: {list(st.secrets.keys())}"
+    )
 
 engine = create_engine(DATABASE_URL)
-
-assert st.secrets.get("DATABASE_URL") is not None
 
 SessionLocal = sessionmaker(
     autocommit=False,
